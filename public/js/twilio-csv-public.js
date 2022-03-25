bc2cfbbccbc967921e0066a701386feaa12a77b5
kalamalahala <solo.driver.bob@gmail.com>
2022-03-25 19:45:31 -0400

    move location and re-pull?

--- public/js/twilio-csv-public.js
+++ public/js/twilio-csv-public.js
@@ -1,9 +1,7 @@
 (function( $ ) {
 	'use strict';
 
-	function confirm_contact() {
-		confirm('Are you sure you want to send messages to $PEOPLE?');
-	}
+
 
 	/**
 	 * All of the code for your public-facing JavaScript source
@@ -34,3 +32,8 @@
 	 */
 
 })( jQuery );
+
+
+function confirm_contact() {
+	confirm('Are you sure you want to send messages to $PEOPLE?');
+}
